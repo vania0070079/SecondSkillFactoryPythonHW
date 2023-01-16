71a1757e66dde5936e3ce0ed1edf56821525c34c
vania0070079 <vania0070079@mail.ru>
2023-01-16 20:11:27 +0300

    SkillFactory HomeWork 13.8.19 (HW-003)

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-money = int(input("Здравствуйте! Введите сумму пополнения: "))
-per_cent = {'ТКБ': 5.6,
-            'СКБ': 5.9,
-            'ВТБ': 4.28,
-            'СБЕР': 4.0}
-deposit = []
-for i in per_cent:
-    deposit.append(per_cent[i] * money / 100)
-max_deposit = max(deposit)
-print(deposit)
-print ("Максимальная сумма, которую вы можете заработать — " + str(max_deposit))
+tickets = int(input("Сколько билетов вы хотите приобрести? "))
+people = tickets
+money = 0
+
+while people != 0:
+    age_for_ticket = int(input(f'Возраст зрителя для билета номер {people}? '))
+    if age_for_ticket < 18:
+        print('Для посетителя конференции возрастом менее 18 лет билет бесплатный.')
+
+    elif 25 > age_for_ticket >= 18:
+        money = money + 990
+        print('Стоимость данного билета: 990 руб.')
+
+    else:
+        money = money + 1390
+        print('Стоимость данного билета: 1390 руб.')
+    people = people - 1
+
+if tickets > 3:
+    sale = money - (money / 10)
+    print(f'Итоговая сумма к оплате {sale} руб., так как применена скидка за покупку более 3 билетов в размере 10%')
+
+else:
+    print(f'Итоговая сумма к оплате {money} руб.')
